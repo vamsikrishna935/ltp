15967b44ddb7c87755ea94f155836cc96bcf9f8b
vamsikrishna935 <velichety.vamsikrishna@wipro.com>
2020-08-08 15:22:31 +0000

    mmap14

--- testcases/kernel/syscalls/mmap/mmap14.c
+++ testcases/kernel/syscalls/mmap/mmap14.c
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 
 		addr = mmap(NULL, MMAPSIZE, PROT_READ | PROT_WRITE,
 			    MAP_PRIVATE | MAP_LOCKED | MAP_ANONYMOUS,
-			    -1, 0);
+			    0, 0);
 
 		if (addr == MAP_FAILED) {
 			tst_resm(TFAIL | TERRNO, "mmap of %s failed", TEMPFILE);
